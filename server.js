8bb1efb37c43d7e6442c158e56eec48c3bba72e2
Nil-132 <nilesh25521@gmail.com>
2026-04-16 15:06:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -301,4 +301,4 @@ app.delete('/api/live-schedules/:id', authenticate, isAdmin, async (req, res) =>
 
 // ====================== SERVER START ======================
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running on ${PORT}`));
